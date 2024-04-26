52d6eee470fa81e05157db9420706efad89d524a
Shiokiri <xluoying2017@gmail.com>
2024-04-27 05:12:44 +0800

    feat: test string

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -6,17 +6,17 @@ fn g(i: f64) -> f64 {
     i * 2.0
 }
 
-fn main() {
+fn hello_world() {
     println!("Hello, world!");
-    let mut x : i32 = 5;
+    let mut x: i32 = 5;
     println!("x:{x}");
     x = 6;
     println!("x:{x}");
     x = f(x);
     println!("x:{x}");
 
-    let mut y : f64 = 1.5;
-    y= g(y);
+    let mut y: f64 = 1.5;
+    y = g(y);
     println!("y:{y}");
 
     let z = 1;
@@ -27,7 +27,7 @@ fn main() {
     }
     println!("z:{z}");
 
-    let tup : (i32, f64, u8) = (500, 6.4, 1);
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
     let t1 = tup.0;
     let t2 = tup.1;
     let t3 = tup.2;
@@ -61,6 +61,23 @@ fn main() {
     for number in (1..10).rev() {
         println!("{number}");
     }
-    
+}
 
+fn say_hello(s: &str) {
+    println!("{}", s);
+}
+
+fn first_word(s: &String) -> &str {
+    &s[..1]
+}
+
+fn main() {
+    let s = String::from("hello,world!");
+    say_hello(&s);
+    say_hello(&s[..]);
+    say_hello(s.as_str());
+
+    let s: String = String::from("hello, world!");
+    let word = first_word(&s);
+    println!("the first word is: {}", word);
 }
